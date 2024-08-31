84b33369e254f8a9a1caa51662f13e63cf83e92c
Nauzoo <nauhanoliveira@gmail.com>
2024-08-30 21:09:43 -0300

    Configurando os terminais e PWM

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -7,6 +7,7 @@
  */
 
 // PS: Os valores dos pinos poderam ser alterados dps;
+
 #define LEFT_ENGINE_A 25
 #define LEFT_ENGINE_B 26
 
@@ -18,33 +19,70 @@
 #define SPIN_FULL_INTENSITY 255
 #define SPIN_HALF_INTENSITY 127
 
-void spinRightEngineFoward(){
+const int right_engine_channel_a = 0; // canal de PWM, desse modo é possivel controlar o GPIO com facilidade
+const int right_engine_channel_b = 1;
+const int left_engine_channel_a = 2;
+const int left_engine_channel_b = 3;
+
+const int PWM_freq = 1000;          // 1Khz  
+const int PWM_res = 8;              // 8bits (0-255)
+
+void spinRightEngineFoward(int speed){
   /* Gira o motor direito na dirção anti-horaria */
   
-  ledcWrite(RIGHT_ENGINE_A, SPIN_FULL_INTENSITY); // Maldade do Vinicius: "usar a funcao ledcWrite ao inves de digital write" 
+  ledcWrite(RIGHT_ENGINE_A, speed); // Maldade do Vinicius: "usar a funcao ledcWrite ao inves de digital write" 
   ledcWrite(RIGHT_ENGINE_B, 0);
 }
 
-void spinRightEngineBackwards(){
+void spinRightEngineBackwards(int speed){
   /* Gira o motor direito na dirção horaria */
   
-  ledcWrite(RIGHT_ENGINE_A, 0); // Maldade do Vinicius: "usar a funcao ledcWrite ao inves de digital write" 
-  ledcWrite(RIGHT_ENGINE_B, SPIN_FULL_INTENSITY);
+  ledcWrite(RIGHT_ENGINE_A, 0);                   // Maldade do Vinicius: "usar a funcao ledcWrite ao inves de digital write" 
+  ledcWrite(RIGHT_ENGINE_B, speed);
+}
+
+void spinLeftEngineFoward(int speed){
+  /* Gira o motor esquerdo na dirção anti-horaria */
+  
+  ledcWrite(LEFT_ENGINE_A, speed); // Maldade do Vinicius: "usar a funcao ledcWrite ao inves de digital write" 
+  ledcWrite(LEFT_ENGINE_B, 0);
+}
+
+void spinLeftEngineBackwards(int speed){
+  /* Gira o motor esquerdo na dirção horaria */
+  
+  ledcWrite(LEFT_ENGINE_A, 0);                   // Maldade do Vinicius: "usar a funcao ledcWrite ao inves de digital write" 
+  ledcWrite(LEFT_ENGINE_B, speed);
 }
 
 
 void setup() {
   // CONFIGURANDO OS PINOS PARA A SAIDA DE DADOS;
-  pinMode(LEFT_ENGINE_A, OUTPUT);
-  pinMode(LEFT_ENGINE_B, OUTPUT);
+
   pinMode(WEAPON_ACTIVATOR, OUTPUT);
 
-  Serial.begin(115200); // Iniciando o monitor serial para debugging
+  ledcSetup(right_engine_channel_a, PWM_freq, PWM_res);
+  ledcSetup(right_engine_channel_b, PWM_freq, PWM_res);
+  ledcSetup(left_engine_channel_a, PWM_freq, PWM_res);
+  ledcSetup(left_engine_channel_b, PWM_freq, PWM_res);
+
+  ledcAttachPin(RIGHT_ENGINE_A, right_engine_channel_a);
+  ledcAttachPin(RIGHT_ENGINE_B, right_engine_channel_b);
+  ledcAttachPin(LEFT_ENGINE_A, left_engine_channel_a);
+  ledcAttachPin(LEFT_ENGINE_B, left_engine_channel_b);
+
+
+  Serial.begin(115200);                   // Iniciando o monitor serial para debugging
   Dabble.begin("ROBOT_NAME_IS_A_SECRET"); // Definindo o nome do bluetooth
 
 }
 
 void loop() {
   Dabble.processInput();  // verifica a cada ciclo se há novos inputs do controle;
+  
+  if (GamePad.isTrianglePressed()){
+    // Ativar arma
+  }
 
+  // Controlar a direcao de movimento pelo analogico
 }
