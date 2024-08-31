33517c711451c372597fa36bd767b07f108b0415
Nauzoo <nauhanoliveira@gmail.com>
2024-08-31 17:03:08 -0300

    adicionando movimento e switch da arma no robo

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,13 +1,11 @@
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
 #include <Arduino.h>
 #include <DabbleESP32.h>
 
 
-/*
-  Definicao dos pinos PWM para o controle de cada um dos motores;
- */
-
+/* Definicao dos pinos PWM para o controle de cada um dos motores; */
 // PS: Os valores dos pinos poderam ser alterados dps;
-
 #define LEFT_ENGINE_A 25
 #define LEFT_ENGINE_B 26
 
@@ -24,42 +22,84 @@ const int right_engine_channel_b = 1;
 const int left_engine_channel_a = 2;
 const int left_engine_channel_b = 3;
 
-const int PWM_freq = 1000;          // 1Khz  
-const int PWM_res = 8;              // 8bits (0-255)
+const int PWM_freq = 1000;            // 1Khz  
+const int PWM_res = 8;                // 8bits (0-255)
 
-void spinRightEngineFoward(int speed){
-  /* Gira o motor direito na dirção anti-horaria */
-  
-  ledcWrite(RIGHT_ENGINE_A, speed); // Maldade do Vinicius: "usar a funcao ledcWrite ao inves de digital write" 
-  ledcWrite(RIGHT_ENGINE_B, 0);
-}
+struct Weapon
+{
+  public:
+    static bool state;
+    
+    static void turnWeaponOn() {
+      digitalWrite(WEAPON_ACTIVATOR, HIGH);
+      state = true;
+    }
 
-void spinRightEngineBackwards(int speed){
-  /* Gira o motor direito na dirção horaria */
-  
-  ledcWrite(RIGHT_ENGINE_A, 0);                   // Maldade do Vinicius: "usar a funcao ledcWrite ao inves de digital write" 
-  ledcWrite(RIGHT_ENGINE_B, speed);
-}
+    static void turnWeaponOff(){
+      digitalWrite(WEAPON_ACTIVATOR, LOW);
+      state = false;
+    }
 
-void spinLeftEngineFoward(int speed){
-  /* Gira o motor esquerdo na dirção anti-horaria */
-  
-  ledcWrite(LEFT_ENGINE_A, speed); // Maldade do Vinicius: "usar a funcao ledcWrite ao inves de digital write" 
-  ledcWrite(LEFT_ENGINE_B, 0);
-}
+    static void togleWeapon(){
+      state ? turnWeaponOff() : turnWeaponOn();
+    }
 
-void spinLeftEngineBackwards(int speed){
-  /* Gira o motor esquerdo na dirção horaria */
+};
+bool Weapon::state = false;
+
+struct Engines
+{
+  /*  PONTE H MX1508 VALORES
+     IN1/IN3 | IN2/IN4 | DIR  
+      
+      LOW    |  LOW    | STANDBY
+      PWM    |  LOW    | FOWARD
+      LOW    |  PWM    | REVERSE
+      HIGH   |  HIGH   | STATIC
+  */
+  public:
+  static void leftEngineSpin(u_int8_t foward, u_int8_t backwards) {
+    ledcWrite(LEFT_ENGINE_A, foward);
+    ledcWrite(LEFT_ENGINE_B, backwards);
+  }
+  static void rightEngineSpin(u_int8_t foward, u_int8_t backwards) {
+    ledcWrite(RIGHT_ENGINE_A, foward);
+    ledcWrite(RIGHT_ENGINE_B, backwards);
+  }
   
-  ledcWrite(LEFT_ENGINE_A, 0);                   // Maldade do Vinicius: "usar a funcao ledcWrite ao inves de digital write" 
-  ledcWrite(LEFT_ENGINE_B, speed);
-}
+};
+
+struct DirectionCore{
+
+  public:
+  static void move(){
+
+    // Otimizações para reduzir a quantidade de condicionais
+
+    u_int8_t pin_left_speed = GamePad.isSquarePressed() ? 0 : SPIN_FULL_INTENSITY;
+    u_int8_t pin_right_speed = GamePad.isCirclePressed() ? 0 : SPIN_FULL_INTENSITY;
+
+    if (GamePad.isUpPressed()) {
+      Engines::leftEngineSpin(pin_left_speed, 0);
+      Engines::rightEngineSpin(pin_right_speed, 0);
+    }
+    else if (GamePad.isDownPressed()){
+      Engines::leftEngineSpin(0, pin_left_speed);
+      Engines::rightEngineSpin(0, pin_right_speed);
+    }
+    else {
+      Engines::leftEngineSpin(pin_left_speed, pin_right_speed);
+      Engines::rightEngineSpin(pin_right_speed, pin_left_speed);
+    }
+  }
+};
 
 
 void setup() {
   // CONFIGURANDO OS PINOS PARA A SAIDA DE DADOS;
 
   pinMode(WEAPON_ACTIVATOR, OUTPUT);
+  Weapon::turnWeaponOff();          // iniciando com a arma desligada.
 
   ledcSetup(right_engine_channel_a, PWM_freq, PWM_res);
   ledcSetup(right_engine_channel_b, PWM_freq, PWM_res);
@@ -71,18 +111,27 @@ void setup() {
   ledcAttachPin(LEFT_ENGINE_A, left_engine_channel_a);
   ledcAttachPin(LEFT_ENGINE_B, left_engine_channel_b);
 
-
   Serial.begin(115200);                   // Iniciando o monitor serial para debugging
   Dabble.begin("ROBOT_NAME_IS_A_SECRET"); // Definindo o nome do bluetooth
 
 }
 
 void loop() {
-  Dabble.processInput();  // verifica a cada ciclo se há novos inputs do controle;
+  Dabble.processInput();              // verifica a cada ciclo se há novos inputs do controle
   
-  if (GamePad.isTrianglePressed()){
-    // Ativar arma
+  if (!Dabble.isAppConnected()){      // Deliga todo o sistema caso o app seja desconectado
+    Weapon::turnWeaponOff();
+    Engines::leftEngineSpin(0, 0);
+    Engines::rightEngineSpin(0, 0);
   }
 
-  // Controlar a direcao de movimento pelo analogico
+  if (GamePad.isStartPressed()){
+    Weapon::togleWeapon();
+    delay(100);                     // solução temporaria, pode ser mudada para um "isTriangleReleased"
+  }
+
+  // Joystick não é uma boa opçao pq n há um meio de caucular precisamente a rotação do robo.
+  // Vamos usar o D-Pad ao invés disso.
+  DirectionCore::move();
+
 }
